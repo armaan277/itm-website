e3f9f6862b9f7ff19ce202ae446c69cfb52b34ff
Armaan <khanarmaan272004@gmail.com>
2023-05-19 12:40:01 +0530

    item4 add many changes

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 700) {
+            $(".navbar").css("background" , "black");
+        }
+  
+        else{
+            $(".navbar").css("background" , "none");  	
+        }
+    })
+  });
+
