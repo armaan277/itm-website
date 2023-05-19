cbf2b9bca9297fa624749ae2934be3df2976d1b6
Armaan <khanarmaan272004@gmail.com>
2023-05-19 16:14:49 +0530

    finally done

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 700) {
-            $(".navbar").css("background" , "black");
-        }
-  
-        else{
-            $(".navbar").css("background" , "none");  	
-        }
-    })
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 700) {
+      $(".navbar").css("background", "black");
+    } else {
+      $(".navbar").css("background", "none");
+    }
   });
-
+});
